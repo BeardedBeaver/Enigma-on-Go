d471ec8900d6a0489b726ace9678c4dbe3b91a3d
linev <linev@programmer.net>
2024-11-23 13:52:01 +0200

    Optimize IOC calculation

--- pkg/cracker/textmetrics.go
+++ pkg/cracker/textmetrics.go
@@ -1,18 +1,19 @@
 package cracker
 
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 // IOC computes Index Of Coincidence metric of a passed
 // English text
 func IOC(text string) float64 {
-	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	var letterCount = make(map[byte]int)
+	var letterCount = make(map[rune]int)
 	for _, char := range text {
-		letterCount[byte(char)] += 1
+		letterCount[char] += 1
 	}
 	var ioc float64
 	textLength := float64(len(text))
 	for _, char := range alphabet {
-		ioc += float64(letterCount[byte(char)]) / textLength *
-			(float64(letterCount[byte(char)]) - 1.0) / (textLength - 1.0)
+		ioc += float64(letterCount[char]) / textLength *
+			(float64(letterCount[char]) - 1.0) / (textLength - 1.0)
 	}
 	return ioc
 }
